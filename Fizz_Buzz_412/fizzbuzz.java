cbd91692c703910bebfe83b8d75e613f409a8fe6
Gopala Krishna H K <gopalakrish0826@gmail.com>
2025-11-07 21:21:27 +0530

    Add files via upload

---
+++ Fizz_Buzz_412/fizzbuzz.java
@@ -0,0 +1,28 @@
+package Fizz_Buzz_412;
+
+import java.util.*;
+
+public class fizzbuzz {
+    public List<String> fizzBuzz(int n) {
+        List<String> result = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                result.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                result.add("Fizz");
+            } else if (i % 5 == 0) {
+                result.add("Buzz");
+            } else {
+                result.add(String.valueOf(i));
+            }
+        }
+        return result;
+    }
+
+    // quick main test
+    public static void main(String[] args) {
+    	fizzbuzz s = new fizzbuzz();
+        System.out.println(s.fizzBuzz(15));
+    }
+}

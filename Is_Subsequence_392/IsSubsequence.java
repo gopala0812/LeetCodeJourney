3c1b876427adc6e3d3c660491fae5d4a7152f73e
Gopala Krishna H K <gopalakrish0826@gmail.com>
2025-11-26 21:16:23 +0530

    Add files via upload

---
+++ Is_Subsequence_392/IsSubsequence.java
@@ -0,0 +1,29 @@
+package Is_Subsequence_392;
+
+import java.util.*;
+
+class IsSubsequence {
+    public boolean isSubsequence(String s, String t) {
+        int i = 0;
+        int j = 0;
+
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+
+        return i == s.length();
+    }
+
+    public static void main(String[] args) {
+        IsSubsequence obj = new IsSubsequence();
+
+        String s = "abc";
+        String t = "ahbgdc";
+
+        boolean result = obj.isSubsequence(s, t);
+        System.out.println("Is \"" + s + "\" a subsequence of \"" + t + "\"? " + result);
+    }
+}
